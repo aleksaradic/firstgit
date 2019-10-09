637c99a16742fb2c3bfab7488cb1257002b312e8
aleksaradic <aleksaradic01@gmail.com>
2019-10-09 10:26:26 +0200

    clean the script

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
@@ -10,9 +8,4 @@ public class HelloWorld : MonoBehaviour
         Debug.Log("Hello World");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
